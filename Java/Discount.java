1e07f30f55f0c0c41ddf5b29f959b62b96fc2ac3
theCodeJuggler <41511797+theCodeJuggler@users.noreply.github.com>
2018-07-22 01:46:21 +0530

    Update Discount.java

--- Java/Discount.java
+++ Java/Discount.java
@@ -3,10 +3,10 @@ class Discount
 {
 	public static void main(String args[])
 	{
-		double newItem = 10000;
-		double discount = newItem*35/100;
-		double newItemPrice = newItem-discount;
+		double newItem = 10000;			//setting the price of the item
+		double discount = newItem*35/100;	//calculating the discount amount
+		double newItemPrice = newItem-discount;	//calculating the new discounted price
 		
-		System.out.print("Discounted Price = Rs. "+newItemPrice);
+		System.out.print("Discounted Price = Rs. "+newItemPrice);	//displaying the discounted price
 	}
 }
