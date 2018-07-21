4c6f480d4ef7d4d98226c0d9c88b9729c0ce6760
theCodeJuggler <41511797+theCodeJuggler@users.noreply.github.com>
2018-07-22 01:18:32 +0530

    Update ArmSeries.java

--- Java/ArmSeries.java
+++ Java/ArmSeries.java
@@ -1,26 +1,31 @@
 //WProgram3
+//finding the armstrong no. falling between 100 to 999
 class ArmSeries
 {
 	public static void main(String args[])
 	{
-		boolean pt;
+		boolean flag;
+		//running loop 100 to 999 and sending each number to armstrong number checking function
 		for(int i=100; i<=999; i++)
 		{
-			pt=arms(i);
+			flag=armstrongCheck(i);
 			if(pt==true)
-				System.out.println(i);
+				System.out.println(i); //if the number is armstrong then displaying it
 		}
 	}
 	
-	static boolean arms(int n)
+	//function to check for armstrong number condition
+	static boolean armstrongCheck(int n)
 	{
 		int c=n, d, s=0;
 		while(c>0)
 		{
-			d=c%10;
-			s=s+d*d*d;
-			c/=10;
+			d=c%10;		//extracting the digits
+			s=s+d*d*d;	//forming the new number
+			c/=10;		//decrementing the loop variable
 		}
+		
+		//checking the armstrong number condition
 		if(n==s)
 			return true;
 		else
