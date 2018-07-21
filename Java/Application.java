7ca161ed9ca2f40007061e74b65ba951f95a5cb4
theCodeJuggler <41511797+theCodeJuggler@users.noreply.github.com>
2018-07-22 01:07:31 +0530

    Update Application.java

--- Java/Application.java
+++ Java/Application.java
@@ -1,4 +1,5 @@
 //WProgram8
+//Program to simulate login into application using user id and password
 import java.io.*;
 class Application
 {
@@ -6,8 +7,11 @@ class Application
 	{
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 		
+		//Setting a predefined user id and password
 		String uid="abbiswas", pwd="12345";
 		int i=1;
+		
+		//using do-while loop to give continuous 3 chances to login in case of wrong id password inserted
 		do
 		{
 			System.out.print("User ID :: "); String id=in.readLine();
@@ -22,6 +26,7 @@ class Application
 		}
 		while(i<=3);
 		
+		//displaying message if the 3 chances get exhausted
 		if(i==4)
 			System.out.println("Contact Admin");
 	}
