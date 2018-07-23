e45dfdbf679bbd959b5b9a8f4a7c24e4077a832c
theCodeJuggler <41511797+theCodeJuggler@users.noreply.github.com>
2018-07-24 00:22:01 +0530

    Update MissingNo.java

--- Java/MissingNo.java
+++ Java/MissingNo.java
@@ -1,4 +1,5 @@
 //TProgram5
+//Finding out missing numbers from 0 to n in an array
 import java.io.*;
 class MissingNo
 {
@@ -6,19 +7,22 @@ class MissingNo
 	{
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 		
+		//Accepting the number of inputs
 		System.out.print("Enter The array size :: "); int size=Integer.parseInt(in.readLine());
 		int ar[]=new int[size];
 		int sum=0;
 		
+		//Accepting the array elements
 		System.out.print("Enter The array elements:: ");
 		for(int i=0;i<size;i++)
 		{
 			ar[i]=Integer.parseInt(in.readLine());
-			sum+=ar[i];
+			sum+=ar[i];				//calculating the sum of the inputted number	
 		}
 		
-		int total = (size+1)*(size)/2;
 		
-		System.out.print("Missing No. = "+(total-sum));
+		int total = (size+1)*(size)/2;			//calculating the sum of numbers from 1 to n
+		
+		System.out.print("Missing No. = "+(total-sum));	//calculating the missing number
 	}
 }
