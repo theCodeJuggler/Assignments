c521012aea8cf54a925fba9e5c0cff7a3435a5db
theCodeJuggler <41511797+theCodeJuggler@users.noreply.github.com>
2018-07-24 22:24:29 +0530

    Update DateTest.java

--- Java/Sts Space/Date/src/DateTest.java
+++ Java/Sts Space/Date/src/DateTest.java
@@ -3,14 +3,14 @@ public class DateTest {
 
 	public static void main(String[] args)
 	{
-		//creating objects and initialising the instance member 
-		Date newDate=new Date(7,7,2018);
-		Date anotherNewDate=new Date(1,1,1990);
+		//creating objects and initialising the instance members 
+		Date newDate=new Date(24,07,2018);
+		Date oldDate=new Date(04,04,1995);
 		
-		int ar[]=newDate.differenceFirstway(anotherNewDate);
+		int ar[]=newDate.diff(oldDate);
 		
 		System.out.println("Date 1:-"+newDate);
-		System.out.println("Date 2:-"+anotherNewDate);
+		System.out.println("Date 2:-"+oldDate);
 		System.out.println("Total Days difference is "+ar[0]+"\nTotal Months difference is "+ar[1]+"\nTotal Years difference is "+ar[2]);
 		
 	}
